d9f7fc7c8d2d8d8c87c1b32518f906854640c2de
ymle <ymle@kibocommerce.com>
2019-01-24 17:21:29 +0700

    test

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
+import App from "./App";
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import todoApp from './reducers/reducers'
 
-ReactDOM.render(<App />, document.getElementById('app'));
+let store = createStore(todoApp)
+let rootElement = document.getElementById('app')
+
+ReactDOM.render(
+    <Provider store = {store}>
+        <App/>
+    </Provider>,
+    rootElement
+)
